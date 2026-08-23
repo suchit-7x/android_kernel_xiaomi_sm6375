3359dbcbaa9f9d72a41b4a443d2fde86ebcf400a
suchit-7x <gtsuchit7x@gmail.com>
2026-08-23 10:12:28 +0530

    input: pm8941-pwekey: Import changes from spring-v-oss

--- drivers/input/misc/pm8941-pwrkey.c
+++ drivers/input/misc/pm8941-pwrkey.c
@@ -19,6 +19,8 @@
 #include <linux/platform_device.h>
 #include <linux/reboot.h>
 #include <linux/regmap.h>
+#include <linux/nmi.h>
+#include <linux/sched/debug.h>
 #include <linux/suspend.h>
 
 #define PON_REV2			0x01
@@ -36,9 +38,10 @@
 #define PON_RT_STS			0x10
 #define  PON_KPDPWR_N_SET		BIT(0)
 #define  PON_RESIN_N_SET		BIT(1)
+#define  PON_KPDPWR_N_BARK_SET		BIT(3)
 #define  PON_GEN3_RESIN_N_SET		BIT(6)
 #define  PON_GEN3_KPDPWR_N_SET		BIT(7)
-
+#define  PON_K_R_BARK_SET 		BIT(5)
 #define PON_PS_HOLD_RST_CTL		0x5a
 #define PON_PS_HOLD_RST_CTL2		0x5b
 #define  PON_PS_HOLD_ENABLE		BIT(7)
@@ -56,6 +59,9 @@
 #define  PON_DBC_DELAY_MASK_GEN2	0xf
 #define  PON_DBC_SHIFT_GEN1		6
 #define  PON_DBC_SHIFT_GEN2		14
+
+#define PMIC_PWRKEY_BARK_TRIGGER	1
+#define PMIC_PWRKEY_TRIGGER		2
 
 struct pm8941_data {
 	unsigned int	pull_up_bit;
@@ -88,6 +94,9 @@ struct pm8941_pwrkey {
 	bool log_kpd_event;
 	const struct pm8941_data *data;
 };
+
+RAW_NOTIFIER_HEAD(pwrkey_irq_notifier_list);
+EXPORT_SYMBOL_GPL(pwrkey_irq_notifier_list);
 
 static int pm8941_reboot_notify(struct notifier_block *nb,
 				unsigned long code, void *unused)
@@ -151,11 +160,81 @@ static int pm8941_reboot_notify(struct notifier_block *nb,
 	return NOTIFY_DONE;
 }
 
+
+typedef int (*mi_display_pwrkey_callback)(int);
+mi_display_pwrkey_callback mi_display_pwrkey_cb = NULL;
+
+void mi_display_pwrkey_callback_set(mi_display_pwrkey_callback cb)
+{
+	mi_display_pwrkey_cb = cb;
+	printk(KERN_INFO "%s: func %pF is set.\n", __func__, cb);
+	pr_err("%s: func %pF is set.\n", __func__, cb);
+	return;
+}
+EXPORT_SYMBOL(mi_display_pwrkey_callback_set);
+
+void show_state_filter_single(unsigned long state_filter)
+{
+	struct task_struct *g, *p;
+
+	pr_err("task PC stack pid father\n");
+	rcu_read_lock();
+	for_each_process_thread(g, p) {
+		/*
+		 * reset the NMI-timeout, listing all files on a slow
+		 * console might take a lot of time:
+		 * Also, reset softlockup watchdogs on all CPUs, because
+		 * another CPU might be blocked waiting for us to process
+		 * an IPI.
+		 */
+		touch_nmi_watchdog();
+		//touch_all_softlockup_watchdogs();
+		if (p->__state == state_filter)
+			sched_show_task(p);
+	}
+	rcu_read_unlock();
+}
+
 static irqreturn_t pm8941_pwrkey_irq(int irq, void *_data)
 {
 	struct pm8941_pwrkey *pwrkey = _data;
 	unsigned int sts;
 	int err;
+	pr_err("%s: func \n", __func__);
+	if (!strcmp(pwrkey->data->name,"pmic_pwrkey_bark")){
+  		err = regmap_read(pwrkey->regmap,
+			pwrkey->baseaddr + PON_RT_STS, &sts);
+		if (err)
+			return IRQ_HANDLED;
+		sts &= pwrkey->data->status_bit;
+		if (sts) {
+			if(mi_display_pwrkey_cb != NULL){
+				pr_err("%s: mi_display_pwrkey_cb \n", __func__);
+				mi_display_pwrkey_cb(PMIC_PWRKEY_BARK_TRIGGER);
+			}
+			raw_notifier_call_chain(&pwrkey_irq_notifier_list, 0, NULL);
+		}
+		return IRQ_HANDLED;
+	}
+
+	if (!strcmp(pwrkey->data->name,"pmic_pwrkey_resin_bark")){
+		err = regmap_read(pwrkey->regmap,
+			pwrkey->baseaddr + PON_RT_STS, &sts);
+		if (err)
+			return IRQ_HANDLED;
+		sts &= pwrkey->data->status_bit;
+
+		if(sts){
+			int tmp_console = console_loglevel;
+			pr_err("pwrkey_resin_bark_irq trigger, start D&R task info");
+			console_loglevel = 15;
+			pr_err("------ collect D&R-state processes info before long comb key ------\n");
+			show_state_filter_single(TASK_UNINTERRUPTIBLE);
+			show_state_filter_single(TASK_RUNNING);
+			pr_err("------ end collecting D&R-state processes info ------\n");
+			console_loglevel = tmp_console;
+		}
+	}
 
 	if (pwrkey->sw_debounce_time_us) {
 		if (ktime_before(ktime_get(), pwrkey->sw_debounce_end_time)) {
@@ -192,6 +271,13 @@ static irqreturn_t pm8941_pwrkey_irq(int irq, void *_data)
 
 	input_report_key(pwrkey->input, pwrkey->code, sts);
 	input_sync(pwrkey->input);
+	pr_err("%s: pwrkey\n", __func__);
+	if(pwrkey->code == KEY_POWER && !sts){
+		pr_err("%s: KEY_POWER\n", __func__);
+		if(mi_display_pwrkey_cb != NULL){
+			mi_display_pwrkey_cb(PMIC_PWRKEY_TRIGGER);
+		}
+	}
 
 	return IRQ_HANDLED;
 }
@@ -314,7 +400,7 @@ static int pm8941_pwrkey_suspend(struct device *dev)
 {
 	struct pm8941_pwrkey *pwrkey = dev_get_drvdata(dev);
 
-	if (pm_suspend_target_state == PM_SUSPEND_MEM)
+	if (pm_suspend_via_firmware())
 		return pm8941_pwrkey_freeze(dev);
 
 	if (device_may_wakeup(dev))
@@ -327,7 +413,7 @@ static int pm8941_pwrkey_resume(struct device *dev)
 {
 	struct pm8941_pwrkey *pwrkey = dev_get_drvdata(dev);
 
-	if (pm_suspend_target_state == PM_SUSPEND_MEM)
+	if (pm_suspend_via_firmware())
 		return pm8941_pwrkey_restore(dev);
 
 	if (device_may_wakeup(dev))
@@ -548,11 +634,32 @@ static const struct pm8941_data pon_gen3_resin_data = {
 	.has_pon_pbs = true,
 };
 
+static const struct pm8941_data pon_gen3_pwrkey_bark_data = {
+  	.pull_up_bit = PON_KPDPWR_PULL_UP,
+	.status_bit = PON_KPDPWR_N_BARK_SET,
+	.name = "pmic_pwrkey_bark",
+	.phys = "pmic_pwrkey_bark/input0",
+	.supports_ps_hold_poff_config = true,
+	.supports_debounce_config = true,
+	.has_pon_pbs = false,
+};
+
+static const struct pm8941_data pon_gen3_pwrkey_resin_bark_data = {
+	.status_bit = PON_K_R_BARK_SET,
+	.name = "pmic_pwrkey_resin_bark",
+	.phys = "pmic_pwrkey_resin_bark/input0",
+	.supports_ps_hold_poff_config = false,
+	.supports_debounce_config = false,
+	.has_pon_pbs = true,
+};
+
 static const struct of_device_id pm8941_pwr_key_id_table[] = {
 	{ .compatible = "qcom,pm8941-pwrkey", .data = &pwrkey_data },
+    { .compatible = "qcom,pmk8350-pwrkey-bark", .data = &pon_gen3_pwrkey_bark_data },
 	{ .compatible = "qcom,pm8941-resin", .data = &resin_data },
 	{ .compatible = "qcom,pmk8350-pwrkey", .data = &pon_gen3_pwrkey_data },
 	{ .compatible = "qcom,pmk8350-resin", .data = &pon_gen3_resin_data },
+	{ .compatible = "qcom,pmk8350-pwrkey-resin-bark", .data = &pon_gen3_pwrkey_resin_bark_data },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, pm8941_pwr_key_id_table);
