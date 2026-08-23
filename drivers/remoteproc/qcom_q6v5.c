7b479bbf37be67eda992f85ede2901d148b12512
suchit-7x <gtsuchit7x@gmail.com>
2026-08-23 09:45:04 +0530

    remoteproc: Import changes from spring-v-oss

--- drivers/remoteproc/qcom_q6v5.c
+++ drivers/remoteproc/qcom_q6v5.c
@@ -15,34 +15,125 @@
 #include <linux/soc/qcom/smem_state.h>
 #include <linux/remoteproc.h>
 #include <linux/delay.h>
-#include <linux/interconnect.h>
-#include <linux/rbtree.h>
-#include <linux/kthread.h>
-#include <linux/fs.h>
-#include <linux/slab.h>
-#include <linux/uaccess.h>
-#include <linux/workqueue.h>
-
 #include "qcom_common.h"
+#include <linux/seq_file.h>
+#include <linux/proc_fs.h>
 #include "qcom_q6v5.h"
 #include <trace/events/rproc_qcom.h>
+//IMEI anti-tampering
+#include <linux/workqueue.h>
+#include <linux/slab.h>
 
 #define Q6V5_PANIC_DELAY_MS	200
-#define MAX_FW_FILE_SIZE (4 * 1024)
-#define NAME_LEN 64
-#define LINE_LEN 128
-#define UUID_LEN 36
-#define SMEM_BUFFER_LEN 4096
 
-#ifdef CONFIG_QCOM_CRASH_SYMBOL_MATCH
-struct symbol_entry {
-	struct rb_node node;
-	u32 addr;
-	const char *name;
+#define MAX_SSR_REASON_LEN	256U
+#define MAX_CRASH_REASON    256
+//IMEI anti-tampering
+#define STR_NV_SIGNATURE_DESTROYED "CRITICAL_DATA_CHECK_FAILED"
+
+static int crash_num = 0;
+static bool ramdump_state = false;
+static char last_modem_sfr_reason[MAX_SSR_REASON_LEN] = "none";
+static struct proc_dir_entry *last_modem_sfr_entry = NULL;
+static struct proc_dir_entry *ramdump_state_sfr_entry = NULL;
+static char modem_crash_reason[MAX_CRASH_REASON][MAX_SSR_REASON_LEN]={"0"};
+//IMEI anti-tampering
+static struct kobject *checknv_kobj;
+static struct kset *checknv_kset;
+
+/* modem crash history entry */
+static int last_modem_sfr_proc_show(struct seq_file *m, void *v)
+{
+	seq_printf(m, "%s\n", last_modem_sfr_reason);
+	return 0;
+}
+static int last_modem_sfr_proc_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, last_modem_sfr_proc_show, NULL);
+}
+static const struct proc_ops last_modem_sfr_file_ops = {
+	//.owner   = THIS_MODULE,
+	.proc_open    = last_modem_sfr_proc_open,
+	.proc_read    = seq_read,
+	.proc_lseek   = seq_lseek,
+	.proc_release = single_release,
+};
+/* ramdump state entry */
+static int ramdump_state_proc_clear(struct seq_file *m, void *v)
+{
+	ramdump_state = true;
+	pr_err("ramdump state change\n");
+	seq_printf(m, "%s\n", "null");
+	return 0;
+}
+static int ramdump_state_proc_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, ramdump_state_proc_clear, NULL);
+}
+static const struct proc_ops ramdump_state_file_ops = {
+	//.owner   = THIS_MODULE,
+	.proc_open    = ramdump_state_proc_open,
+	.proc_read    = seq_read,
+	.proc_lseek   = seq_lseek,
+	.proc_release = single_release,
+};
+/* IMEI anti-tampering start */
+static const struct sysfs_ops checknv_sysfs_ops = {
 };
 
-static struct rb_root symbol_tree = RB_ROOT;
-#endif
+static void kobj_release(struct kobject *kobj)
+{
+	kfree(kobj);
+}
+static struct kobj_type checknv_ktype = {
+	.sysfs_ops = &checknv_sysfs_ops,
+	.release = kobj_release,
+};
+static void checknv_kobj_clean(struct work_struct *work)
+{
+	kobject_uevent(checknv_kobj, KOBJ_REMOVE);
+	kobject_put(checknv_kobj);
+	kset_unregister(checknv_kset);
+}
+static void checknv_kobj_create(struct work_struct *work)
+{
+	int ret;
+	if (checknv_kobj && checknv_kset != NULL) {
+		pr_info("checknv_kset is not NULL, should clean up.");
+		kobject_uevent(checknv_kobj, KOBJ_REMOVE);
+		kobject_put(checknv_kobj);
+	}
+	checknv_kobj = kzalloc(sizeof(struct kobject), GFP_KERNEL);
+	if (!checknv_kobj) {
+		pr_info("kobject alloc failed.");
+		return;
+	}
+	if (checknv_kset == NULL) {
+		checknv_kset = kset_create_and_add("checknv_errimei", NULL, NULL);
+		if (!checknv_kset) {
+			pr_err("kset creation failed.");
+			goto free_kobj;
+		}
+	}
+	checknv_kobj->kset = checknv_kset;
+	ret = kobject_init_and_add(checknv_kobj, &checknv_ktype, NULL, "%s", "errimei");
+	if (ret) {
+		pr_info("%s: Error in creation kobject", __func__);
+		goto del_kobj;
+	}
+	pr_info("IMEI anti-tampering start");
+	kobject_uevent(checknv_kobj, KOBJ_ADD);
+	pr_info("IMEI anti-tampering start success");
+	return;
+del_kobj:
+	kobject_put(checknv_kobj);
+	kset_unregister(checknv_kset);
+free_kobj:
+	kfree(checknv_kobj);
+}
+static DECLARE_DELAYED_WORK(create_kobj_work, checknv_kobj_create);
+static DECLARE_WORK(clean_kobj_work, checknv_kobj_clean);
+/* IMEI anti-tampering end */
 
 /**
  * qcom_q6v5_prepare() - reinitialize the qcom_q6v5 context before start
@@ -52,6 +143,19 @@ static struct rb_root symbol_tree = RB_ROOT;
  */
 int qcom_q6v5_prepare(struct qcom_q6v5 *q6v5)
 {
+	if (last_modem_sfr_entry == NULL) {
+		last_modem_sfr_entry = proc_create("last_mcrash", S_IFREG | S_IRUGO, NULL, &last_modem_sfr_file_ops);
+	}
+	if (!last_modem_sfr_entry) {
+		printk(KERN_ERR "pil: cannot create proc entry last_mcrash\n");
+	}
+	if (ramdump_state_sfr_entry == NULL) {
+		ramdump_state_sfr_entry = proc_create("ramdump_state", S_IFREG | S_IRUGO | S_IWUGO, NULL, &ramdump_state_file_ops);
+	}
+	if (!ramdump_state_sfr_entry) {
+		printk(KERN_ERR "pil: cannot create proc entry ramdump_state\n");
+	}
+
 	reinit_completion(&q6v5->start_done);
 	reinit_completion(&q6v5->stop_done);
 
@@ -114,181 +218,13 @@ static void qcom_q6v5_crash_handler_work(struct work_struct *work)
 	panic("Panicking, remoteproc %s crashed\n", q6v5->rproc->name);
 }
 
-#ifdef CONFIG_QCOM_CRASH_SYMBOL_MATCH
-static char *read_symbol_file(struct qcom_q6v5 *q6v5, const char *path, size_t *size_out)
-{
-	char *buf;
-	int ret;
-	const struct firmware *symtab = NULL;
-
-	ret = request_firmware(&symtab, path, q6v5->dev);
-	if (ret < 0) {
-		dev_err(q6v5->dev, "request_firmware failed: %s (%ld)\n", path, ret);
-		return ERR_PTR(ret);
-	}
-	buf = kvzalloc(symtab->size + 1, GFP_KERNEL);
-	if (!buf) {
-		release_firmware(symtab);
-		return ERR_PTR(-ENOMEM);
-	}
-	if (symtab->data < 0) {
-		dev_err(q6v5->dev, "Firmware is empty or invalid: %zd\n", symtab->data);
-		kvfree(buf);
-		release_firmware(symtab);
-		return ERR_PTR(-EINVAL);
-	}
-	memcpy(buf, symtab->data, symtab->size);
-	*size_out = symtab->size;
-	release_firmware(symtab);
-	return buf;
-}
-
-static int parse_symbols(struct qcom_q6v5 *q6v5, const char *buf, size_t size)
-{
-	const char *cur = buf;
-	const char *end = buf + size;
-	const char *line_start;
-	char line[LINE_LEN];
-	char name[NAME_LEN];
-	uint32_t addr;
-	int32_t len;
-	struct symbol_entry *entry, *this;
-	struct rb_node **new;
-	struct rb_node *parent;
-
-	while (cur < end) {
-		len = 0;
-		line_start = cur;
-
-		while (cur < end && *cur != '\n')
-			cur++;
-
-		len = min((int)(cur - line_start), (int)(sizeof(line) - 1));
-		memcpy(line, line_start, len);
-		line[len] = '\0';
-
-		if (cur < end)
-			cur++;
-
-		if (sscanf(line, "%x %63s", &addr, name) == 2) {
-			entry = kzalloc(sizeof(*entry), GFP_KERNEL);
-			if (!entry)
-				return -ENOMEM;
-			entry->addr = addr;
-			entry->name = kstrdup(name, GFP_KERNEL);
-			if (!entry->name) {
-				kfree(entry);
-				return -ENOMEM;
-			}
-			new = &symbol_tree.rb_node;
-			parent = NULL;
-			while (*new) {
-				this = rb_entry(*new, struct symbol_entry, node);
-				parent = *new;
-				if (addr < this->addr)
-					new = &(*new)->rb_left;
-				else
-					new = &(*new)->rb_right;
-			}
-			rb_link_node(&entry->node, parent, new);
-			rb_insert_color(&entry->node, &symbol_tree);
-		}
-	}
-	return 0;
-}
-
-static const char *match_function(u32 addr)
-{
-	struct rb_node *node = symbol_tree.rb_node;
-	const char *closest = "none";
-
-	while (node) {
-		struct symbol_entry *entry = rb_entry(node, struct symbol_entry, node);
-
-		if (entry->addr == addr)
-			return entry->name;
-		else if (entry->addr < addr) {
-			closest = entry->name;
-			node = node->rb_right;
-		} else {
-			node = node->rb_left;
-		}
-	}
-	return closest;
-}
-
-static void symbol_loader_work(struct work_struct *work)
-{
-	size_t len;
-	char *buf, *cur, *msg, *end, *token, *callstack_entry, *addr_start;
-	const char *func;
-	char uuid[UUID_LEN + 1];
-	char path[LINE_LEN];
-	size_t size;
-	uint32_t addr;
-	int ret;
-	struct qcom_q6v5 *q6v5;
-
-	q6v5 = container_of(work, struct qcom_q6v5, symbol_loader);
-	msg = qcom_smem_get(q6v5->smem_host_id, q6v5->crash_stack, &len);
-	if (IS_ERR(msg) || len < UUID_LEN) {
-		dev_err(q6v5->dev, "Failed to get UUID from crash_stack\n");
-		return;
-	}
-
-	if (len < (UUID_LEN + 1)) {
-		dev_err(q6v5->dev, "Not enough data for UUID: %zu\n", len);
-		return;
-	}
-
-	memcpy(uuid, msg + (len - (UUID_LEN + 1)), UUID_LEN);
-	uuid[UUID_LEN] = '\0';
-
-	snprintf(path, sizeof(path), "%s_symtab.txt", uuid);
-	buf = read_symbol_file(q6v5, path, &size);
-	if (IS_ERR(buf))
-		return;
-	ret = parse_symbols(q6v5, buf, size);
-	kvfree(buf);
-	if (ret) {
-		dev_err(q6v5->dev, "Failed to parse symbols\n");
-		return;
-	}
-
-	if (q6v5->crash_stack) {
-		cur = msg;
-		end = msg + len;
-		token = strsep(&cur, "|");	/* do this once to get rid of the header */
-		dev_err(q6v5->dev, "Stack Trace:\n");
-		while (cur && cur < end) {
-			callstack_entry = strsep(&cur, "|");
-			if (!callstack_entry)
-				break;
-			addr_start = strpbrk(callstack_entry, ")");
-			if (!addr_start)
-				break;
-			token = addr_start + 1;
-			if (token[0] == '\0')
-				continue;
-
-			if (kstrtou32(token, 16, &addr) == 0) {
-				func = match_function(addr);
-				dev_err(q6v5->dev, "%s (0x%08x)\n", func, addr);
-			}
-		}
-	}
-}
-#endif
-
-
 static irqreturn_t q6v5_wdog_interrupt(int irq, void *data)
 {
 	struct qcom_q6v5 *q6v5 = data;
 	size_t len;
 	char *msg;
-
-	if (q6v5->early_boot && !completion_done(&q6v5->subsys_booted))
-		complete(&q6v5->subsys_booted);
+  	char reason[MAX_SSR_REASON_LEN];
+  	const char *name = q6v5->rproc->firmware;
 
 	/* Sometimes the stop triggers a watchdog rather than a stop-ack */
 	if (!q6v5->running) {
@@ -301,9 +237,20 @@ static irqreturn_t q6v5_wdog_interrupt(int irq, void *data)
 	msg = qcom_smem_get(QCOM_SMEM_HOST_ANY, q6v5->crash_reason, &len);
 	if (!IS_ERR(msg) && len > 0 && msg[0]) {
 		dev_err(q6v5->dev, "watchdog received: %s\n", msg);
+		dev_err(q6v5->dev, "subsystem failure reason: %s. \n", msg);
+
+		strlcpy(reason,msg, MAX_SSR_REASON_LEN);
+		snprintf(last_modem_sfr_reason, (size_t)MAX_SSR_REASON_LEN, "%s: %s", name, reason);
+
 		trace_rproc_qcom_event(dev_name(q6v5->dev), "q6v5_wdog", msg);
+
+		if (ramdump_state){
+			strlcpy(modem_crash_reason[crash_num++], msg, MAX_SSR_REASON_LEN);
+		}
+
 	} else {
 		dev_err(q6v5->dev, "watchdog without message\n");
+		dev_err(q6v5->dev, "subsystem failure reason: watchdog without message. \n");
 	}
 
 	q6v5->running = false;
@@ -328,9 +275,8 @@ static irqreturn_t q6v5_fatal_interrupt(int irq, void *data)
 	struct qcom_q6v5 *q6v5 = data;
 	size_t len;
 	char *msg;
-
-	if (q6v5->early_boot && !completion_done(&q6v5->subsys_booted))
-		complete(&q6v5->subsys_booted);
+  	char reason[MAX_SSR_REASON_LEN];
+  	const char *name = q6v5->rproc->firmware;
 
 	if (!q6v5->running) {
 		dev_info(q6v5->dev, "received fatal irq while q6 is offline\n");
@@ -341,32 +287,39 @@ static irqreturn_t q6v5_fatal_interrupt(int irq, void *data)
 	msg = qcom_smem_get(QCOM_SMEM_HOST_ANY, q6v5->crash_reason, &len);
 	if (!IS_ERR(msg) && len > 0 && msg[0]) {
 		dev_err(q6v5->dev, "fatal error received: %s\n", msg);
+		dev_err(q6v5->dev, "subsystem failure reason: %s. \n", msg);
+          	strlcpy(reason, msg, MAX_SSR_REASON_LEN);
+          	snprintf(last_modem_sfr_reason, (size_t)MAX_SSR_REASON_LEN, "%s: %s", name, reason);
+
 		trace_rproc_qcom_event(dev_name(q6v5->dev), "q6v5_fatal", msg);
+
+		if (ramdump_state){
+			strlcpy(modem_crash_reason[crash_num++], msg, MAX_SSR_REASON_LEN);
+		}
 	} else {
 		dev_err(q6v5->dev, "fatal error without message\n");
+		dev_err(q6v5->dev, "subsystem failure reason: fatal error without message. \n");
 	}
 
-#ifdef CONFIG_QCOM_CRASH_SYMBOL_MATCH
-	if (queue_work(system_freezable_wq, &q6v5->symbol_loader)) {
-		dev_info(q6v5->dev, "Symbol loader work started\n");
-		flush_work(&q6v5->symbol_loader);
+	//IMEI anti-tampering
+	strlcpy(last_modem_sfr_reason, msg, min(len, (size_t)MAX_SSR_REASON_LEN));
+	if (strnstr(last_modem_sfr_reason, STR_NV_SIGNATURE_DESTROYED, strlen(last_modem_sfr_reason))) {
+		pr_err("errimei_dev: the NV has been destroyed, should restart to recovery\n");
+		schedule_delayed_work(&create_kobj_work, msecs_to_jiffies(1*1000));
 	} else {
-		dev_err(q6v5->dev, "Failed to queue symbol loader work\n");
+		q6v5->running = false;
+		dev_err(q6v5->dev, "rproc recovery state: %s\n",
+			q6v5->rproc->recovery_disabled ? "disabled and lead to device crash" :
+			"enabled and kick reovery process");
+
+		if (q6v5->ssr_subdev)
+			qcom_notify_early_ssr_clients(q6v5->ssr_subdev);
+
+		if (q6v5->rproc->recovery_disabled)
+			schedule_work(&q6v5->crash_handler);
+		else
+			rproc_report_crash(q6v5->rproc, RPROC_FATAL_ERROR);
 	}
-#endif
-
-	q6v5->running = false;
-	dev_err(q6v5->dev, "rproc recovery state: %s\n",
-		q6v5->rproc->recovery_disabled ? "disabled and lead to device crash" :
-		"enabled and kick reovery process");
-
-	if (q6v5->ssr_subdev)
-		qcom_notify_early_ssr_clients(q6v5->ssr_subdev);
-
-	if (q6v5->rproc->recovery_disabled)
-		schedule_work(&q6v5->crash_handler);
-	else
-		rproc_report_crash(q6v5->rproc, RPROC_FATAL_ERROR);
 
 	return IRQ_HANDLED;
 }
@@ -376,9 +329,6 @@ static irqreturn_t q6v5_ready_interrupt(int irq, void *data)
 	struct qcom_q6v5 *q6v5 = data;
 
 	complete(&q6v5->start_done);
-
-	if (q6v5->early_boot && !completion_done(&q6v5->subsys_booted))
-		complete(&q6v5->subsys_booted);
 
 	return IRQ_HANDLED;
 }
@@ -397,6 +347,9 @@ int qcom_q6v5_wait_for_start(struct qcom_q6v5 *q6v5, int timeout)
 	int ret;
 
 	ret = wait_for_completion_timeout(&q6v5->start_done, timeout);
+	if (!ret)
+		disable_irq(q6v5->handover_irq);
+
 	return !ret ? -ETIMEDOUT : 0;
 }
 EXPORT_SYMBOL_GPL(qcom_q6v5_wait_for_start);
@@ -405,18 +358,8 @@ static irqreturn_t q6v5_handover_interrupt(int irq, void *data)
 {
 	struct qcom_q6v5 *q6v5 = data;
 
-	if (q6v5->handover_issued) {
-		dev_err(q6v5->dev, "Handover signaled, but it already happened\n");
-		return IRQ_HANDLED;
-	}
-
 	if (q6v5->handover)
 		q6v5->handover(q6v5);
-
-	if (q6v5->early_boot && !completion_done(&q6v5->subsys_booted))
-		complete(&q6v5->subsys_booted);
-
-	icc_set_bw(q6v5->path, 0, 0);
 
 	q6v5->handover_issued = true;
 
@@ -442,6 +385,15 @@ static irqreturn_t q6v5_stop_interrupt(int irq, void *data)
 int qcom_q6v5_request_stop(struct qcom_q6v5 *q6v5, struct qcom_sysmon *sysmon)
 {
 	int ret;
+
+	if (last_modem_sfr_entry) {
+		remove_proc_entry("last_mcrash", NULL);
+		last_modem_sfr_entry = NULL;
+	}
+	if (ramdump_state_sfr_entry) {
+		remove_proc_entry("ramdump_state", NULL);
+		ramdump_state_sfr_entry = NULL;
+	}
 
 	q6v5->running = false;
 
@@ -480,96 +432,6 @@ unsigned long qcom_q6v5_panic(struct qcom_q6v5 *q6v5)
 }
 EXPORT_SYMBOL_GPL(qcom_q6v5_panic);
 
-static irqreturn_t q6v5_pong_interrupt(int irq, void *data)
-{
-	struct qcom_q6v5 *q6v5 = NULL;
-
-	q6v5 = data;
-
-	if (!q6v5) {
-		pr_err("Could not get driver data\n");
-		return -ENODEV;
-	}
-
-	complete(&q6v5->ping_done);
-
-	return IRQ_HANDLED;
-}
-
-int ping_subsystem(struct qcom_q6v5 *q6v5)
-{
-	int ret = -ETIMEDOUT;
-	int ping_failed = 0;
-
-	reinit_completion(&q6v5->ping_done);
-
-	/* Set master kernel Ping bit */
-	ret = qcom_smem_state_update_bits(q6v5->ping_state,
-			    0xffffffff,
-			    BIT(q6v5->ping_bit));
-	if (ret) {
-		dev_err(q6v5->dev, "Failed to update ping bits\n");
-		return ret;
-	}
-
-	ret = wait_for_completion_timeout(&q6v5->ping_done, msecs_to_jiffies(PING_TIMEOUT));
-	if (!ret) {
-		ping_failed = -ETIMEDOUT;
-		dev_err(q6v5->dev, "Failed to get back pong\n");
-	}
-
-
-	/* Clear ping bit master kernel */
-	ret = qcom_smem_state_update_bits(q6v5->ping_state,
-			    BIT(q6v5->ping_bit),
-			    0);
-	if (ret) {
-		pr_err("Failed to clear master kernel bits\n");
-		return ret;
-	}
-	if (ping_failed)
-		return ping_failed;
-
-	return ret;
-
-}
-EXPORT_SYMBOL_GPL(ping_subsystem);
-
-int ping_subsystem_init(struct qcom_q6v5 *q6v5, struct platform_device *pdev)
-{
-	int ret = -ENODEV;
-
-	if (!q6v5) {
-		pr_err("could not get q6v5 data\n");
-		return -ENODEV;
-	}
-
-	q6v5->ping_state = devm_qcom_smem_state_get(&pdev->dev,
-							"ping", &q6v5->ping_bit);
-	if (IS_ERR(q6v5->ping_state)) {
-		pr_err("failed to acquire smem state %ld\n", PTR_ERR(q6v5->ping_state));
-		ret = -ENODEV;
-		return ret;
-	}
-
-	q6v5->pong_irq = platform_get_irq_byname(pdev, "pong");
-	if (q6v5->pong_irq < 0) {
-		pr_err("failed to acquire pong irq\n");
-		ret = -ENODEV;
-		return ret;
-	}
-
-	ret = devm_request_threaded_irq(&pdev->dev, q6v5->pong_irq, NULL,
-			q6v5_pong_interrupt, IRQF_TRIGGER_RISING | IRQF_ONESHOT,
-			"q6v5 pong", q6v5);
-	if (ret)
-		pr_err("failed to acquire pong IRQ\n");
-
-	init_completion(&q6v5->ping_done);
-	return ret;
-}
-EXPORT_SYMBOL_GPL(ping_subsystem_init);
-
 /**
  * qcom_q6v5_init() - initializer of the q6v5 common struct
  * @q6v5:	handle to be initialized
@@ -581,20 +443,29 @@ EXPORT_SYMBOL_GPL(ping_subsystem_init);
  * Return: 0 on success, negative errno on failure
  */
 int qcom_q6v5_init(struct qcom_q6v5 *q6v5, struct platform_device *pdev,
-		struct rproc *rproc, int crash_reason, int crash_stack,
-		unsigned int smem_host_id, bool early_boot,
-		void (*handover)(struct qcom_q6v5 *q6v5))
+		   struct rproc *rproc, int crash_reason,
+		   void (*handover)(struct qcom_q6v5 *q6v5))
 {
 	int ret;
 	struct resource *res;
 
+	if (last_modem_sfr_entry == NULL) {
+		last_modem_sfr_entry = proc_create("last_mcrash", S_IFREG | S_IRUGO, NULL, &last_modem_sfr_file_ops);
+	}
+	if (!last_modem_sfr_entry) {
+		printk(KERN_ERR "pil: cannot create proc entry last_mcrash\n");
+	}
+	if (ramdump_state_sfr_entry == NULL) {
+		ramdump_state_sfr_entry = proc_create("ramdump_state", S_IFREG | S_IRUGO | S_IWUGO, NULL, &ramdump_state_file_ops);
+	}
+	if (!ramdump_state_sfr_entry) {
+		printk(KERN_ERR "pil: cannot create proc entry ramdump_state\n");
+	}
+
 	q6v5->rproc = rproc;
 	q6v5->dev = &pdev->dev;
 	q6v5->crash_reason = crash_reason;
-	q6v5->crash_stack = crash_stack;
-	q6v5->smem_host_id = smem_host_id;
 	q6v5->handover = handover;
-	q6v5->early_boot = early_boot;
 	q6v5->ssr_subdev = NULL;
 
 	init_completion(&q6v5->start_done);
@@ -683,9 +554,6 @@ int qcom_q6v5_init(struct qcom_q6v5 *q6v5, struct platform_device *pdev,
 
 	INIT_WORK(&q6v5->crash_handler, qcom_q6v5_crash_handler_work);
 
-#ifdef CONFIG_QCOM_CRASH_SYMBOL_MATCH
-	INIT_WORK(&q6v5->symbol_loader, symbol_loader_work);
-#endif
 	return 0;
 }
 EXPORT_SYMBOL_GPL(qcom_q6v5_init);
